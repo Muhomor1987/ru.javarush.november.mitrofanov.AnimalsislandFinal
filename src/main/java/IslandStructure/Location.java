bcb7090bef0b0bbe9fee2d566698bde83b1e3d76
Mitrofanov Mikhail <z.pack@mail.ru>
2023-01-17 23:52:34 +0300

    lock unlock

--- src/main/java/IslandStructure/Location.java
+++ src/main/java/IslandStructure/Location.java
@@ -6,10 +6,8 @@ import lombok.Getter;
 import lombok.Setter;
 import util.FabricOfAnimals;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -22,63 +20,77 @@ public class Location implements Runnable {
     final Lock lock = new ReentrantLock();
     Boolean created = false;
     boolean isLiveLocation = false;
-    ArrayList<Animal> animalsOnLocation = new ArrayList<>();
-    ArrayBlockingQueue<Animal> animalsForMoving = new ArrayBlockingQueue<>();
-    ArrayList<Animal> plantSet = new ArrayList<>();
+    ConcurrentLinkedQueue<Animal> animalsOnLocation = new ConcurrentLinkedQueue<>();
+    ConcurrentLinkedQueue<Animal> animalsForMoving = new ConcurrentLinkedQueue<>();
+    ConcurrentLinkedQueue<Animal> plantSet = new ConcurrentLinkedQueue<>();
 
     HashMap<Organisms, Integer> countAnimalsMapOnLocation;
 
     //засыпает на 2 секунды через константа жизненый цикл
-    synchronized public void liveLocation() {
+    public void liveLocation() {
         while (true) {//заменить на wait
-            while (!isLiveLocation) {
-                while (animalsOnLocation.size() > 1) {
-                    Animal animalFirst = animalsOnLocation.remove(0);
-                    Animal animalSecond = animalsOnLocation.remove(0);
-                    Organisms animalFirstTYPE = animalFirst.getTYPE();
-                    Organisms animalSecondTYPE = animalSecond.getTYPE();
-                    //является ли животные одного вида
-                    reproduction(animalFirst, animalSecond);
-                    eat(animalFirst, animalSecond, animalFirstTYPE, animalSecondTYPE);
-
-
-                    //поесть травы
-                    //sleep(100);
-                    //move(animalFirst, animalSecond)
-                    //AnimalForAdd in Location лочим этот лист
+            try {
+                lock.lock();
+                Animal animalFirst = animalsOnLocation.poll();
+                if (animalFirst == null) {
+                    wait();
                 }
+                Animal animalSecond = animalsOnLocation.poll();
+                if (animalSecond == null) {
+                    wait();
+                }
+                assert animalFirst != null;
+                Organisms animalFirstTYPE = animalFirst.getTYPE();
+                assert animalSecond != null;
+                Organisms animalSecondTYPE = animalSecond.getTYPE();
+                //является ли животные одного вида
+                lock.unlock();
+                reproduction(animalFirst, animalSecond);
+
+                eat(animalFirst, animalSecond, animalFirstTYPE, animalSecondTYPE);
+
+
+                //поесть травы
+
+                //move(animalFirst, animalSecond)
+
+                //AnimalForAdd in Location лочим этот лист
+                //sleep(100);
+                lock.unlock();
+
+                wait(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         }
     }
-        synchronized private void eatPlant(Animal animal1, Animal animal2) {
 
-        }
-    synchronized private void eat(Animal animalFirst, Animal animalSecond, Organisms animalFirstTYPE, Organisms animalSecondTYPE) {
+    private void eatPlant(Animal animalFirst, Animal animalSecond) {
+
+
+    }
+
+     private void eat(Animal animalFirst, Animal animalSecond, Organisms animalFirstTYPE, Organisms animalSecondTYPE) {
         if (animalFirst.getMAP_OF_FOOD().containsKey(animalSecondTYPE)) {
-            if (ThreadLocalRandom.current().nextInt(100) >= animalFirst.getMAP_OF_FOOD().get(animalSecondTYPE)) {
-                if(animalSecond.getWeight()>=animalFirst.getSATURATION()){
-                    animalFirst.setWeight(animalFirst.getWEIGHT_MAX());
-                }
-                else {
-                    animalFirst.setWeight(animalFirst.getWeight()+animalSecond.getWeight());
-                }
-                animalFirst.setWeight(animalFirst.getWeight() + animalSecond.getWeight());
-                animalSecond.setLife(false);
-                fabricOfAnimals.getPoolAnimals().get(animalSecondTYPE).add(animalSecond);//Добавляем животное в пулл фабрики
-
-            }
-        }
-        if (animalSecond.getMAP_OF_FOOD().containsKey(animalFirstTYPE)) {
-            if (ThreadLocalRandom.current().nextInt(100) >= animalSecond.getMAP_OF_FOOD().get(animalFirstTYPE)) {
-
-                fabricOfAnimals.getPoolAnimals().get(animalFirstTYPE).add(animalFirst);
-                animalsForMoving.add(animalFirst);
-                
-            }
+            whoEating(animalSecond, animalFirst, animalSecondTYPE);
+        } else if (animalSecond.getMAP_OF_FOOD().containsKey(animalFirstTYPE)) {
+            whoEating(animalFirst, animalSecond, animalFirstTYPE);
         }
     }
 
-    synchronized private void reproduction(Animal animalFirst, Animal animalSecond) {
+    private void whoEating(Animal animalFirst, Animal animalSecond, Organisms animalFirstTYPE) {
+        if (ThreadLocalRandom.current().nextInt(100) >= animalSecond.getMAP_OF_FOOD().get(animalFirstTYPE)) {
+            if (animalFirst.getWeight() >= animalSecond.getSATURATION()) {
+                animalSecond.setWeight(animalSecond.getWEIGHT_MAX());
+            } else {
+                animalSecond.setWeight(animalSecond.getWeight() + animalFirst.getWeight());
+            }
+            animalFirst.setLife(false);
+            fabricOfAnimals.getPoolAnimals().get(animalFirstTYPE).add(animalFirst);
+        }
+    }
+
+    private void reproduction(Animal animalFirst, Animal animalSecond) {
         if (animalFirst.getTYPE() == animalSecond.getTYPE()) {
             fabricOfAnimals.createNewAnimals(this, animalFirst.getTYPE(), statistics);
         }
